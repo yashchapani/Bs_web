344b7d43bce3e3b6210b8f1981a08cf39f63511e
dolphin1999 <paliwalmanu99@gmail.com>
2019-05-25 10:44:17 +0530

    slider and 2nd class modified

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,5 @@
-var slideIndex = 1;
+/*var slideIndex = 1;
+
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -14,4 +15,30 @@ function showDivs(n) {
     x[i].style.display = "none"; 
   }
   x[slideIndex-1].style.display = "block"; 
+  setTimeout(showDivs(slideIndex++),3000);
+}
+*/
+
+var slideIndex = 0;
+var clear;
+showSlide(0);
+
+function clearTime(){
+  clearTimeout(clear,4000);
+}
+
+function showSlide(n) {
+  var i;
+  var x = document.getElementsByClassName("slider1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex += n;
+ 
+  if (slideIndex >= x.length) {slideIndex = 0} ;
+  if (slideIndex < 0) {slideIndex = x.length-1} ;
+  x[slideIndex].style.display = "block";
+   
+  clear = setTimeout(function(){showSlide(1)}, 4000);
+
 }
